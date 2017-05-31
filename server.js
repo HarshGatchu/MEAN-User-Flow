c7833110e486bd162b1658b3575ff1ac3e69ded8
Harsh Patel <Harsh Patel>
2017-05-31 16:18:03 -0700

    First Commit

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
+var restify = require('restify');
 
+
+//Creating Restify Server 
+
+//global.restifyserver= restify.createServer({name: config.name, version:config.version});
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false, mapParams:true }));
 app.use(bodyParser.json());
 app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
 
+
+//Middleware
+//restifyserver.use(restify.jsonBodyParser({mapParams:true}))
+//restifyserver.use(restify.acceptParser(restifyserver.acceptable))
+//restifyserver.use(restify.queryParser({mapParams:true}))
+//restifyserver.use(restify.fullResponse())
 // use JWT auth to secure the api
 app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
 
